474e74ecd23c9e09371bde4160ab25bae881d607
lw4z <ivanvalentimsantos@gmail.com>
2014-10-28 00:07:00 -0300

    Implementação do servlet listarlistaEstabelecimento

--- src/fafica/listaacessivel/ui/ListarListaEstabelecimentoServlet.java
+++ src/fafica/listaacessivel/ui/ListarListaEstabelecimentoServlet.java
@@ -1,11 +1,21 @@
 package fafica.listaacessivel.ui;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fafica.listaacessivel.negocios.Fachada;
+import fafica.listaacessivel.negocios.IFachada;
+import fafica.listaacessivel.negocios.entidades.Estabelecimento;
+import fafica.listaacessivel.negocios.entidades.Lista;
 
 /**
  * Servlet implementation class ListarListaEstabelecimentoServlet
@@ -33,7 +43,38 @@ public class ListarListaEstabelecimentoServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		HttpSession session = request.getSession();
+		Estabelecimento estabelecimento = (Estabelecimento)session.getAttribute("acessoEstabelecimento");
+		Estabelecimento pesquisa = new Estabelecimento();
+		
+		IFachada fachada;
+		
+		if(estabelecimento == null){
+			response.sendRedirect("index.jsp");
+		}else{
+			try {
+				fachada = Fachada.getInstance();
+				pesquisa = fachada.pesquisarEstabelecimento(estabelecimento);
+				List<Lista> listas = fachada.listarListasDoEstabelecimento(pesquisa);
+				
+				request.setAttribute("listasdoestabelecimento", listas);
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("visaoEs.jsp");
+				requestDispatcher.forward(request, response);
+				
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+		
+		
+		
 	}
 
 }
