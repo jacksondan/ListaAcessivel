dd615ec9bc8766e9c78288f64cfafd06e9660320
lw4z <ivanvalentimsantos@gmail.com>
2014-10-27 23:46:07 -0300

    Implementação dos servlet listar listas de cliente e estabelecimento

---
+++ src/fafica/listaacessivel/ui/ListarListaClienteServlet.java
@@ -0,0 +1,39 @@
+package fafica.listaacessivel.ui;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListarListaClienteServlet
+ */
+@WebServlet("/ListarListaClienteServlet")
+public class ListarListaClienteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListarListaClienteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
