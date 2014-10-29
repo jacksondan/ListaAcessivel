5feead9ef0064601f071f33c39d43f15d2c21079
lw4z <ivanvalentimsantos@gmail.com>
2014-10-28 23:33:34 -0300

    Implementação do servlet editarsenharcliente

---
+++ src/fafica/listaacessivel/ui/EditarSenhaClienteServlet.java
@@ -0,0 +1,92 @@
+package fafica.listaacessivel.ui;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import sun.misc.BASE64Encoder;
+import fafica.listaacessivel.negocios.Fachada;
+import fafica.listaacessivel.negocios.IFachada;
+import fafica.listaacessivel.negocios.entidades.Cliente;
+
+/**
+ * Servlet implementation class EditarSenhaClienteServlet
+ */
+@WebServlet("/EditarSenhaClienteServlet")
+public class EditarSenhaClienteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditarSenhaClienteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Cliente cliente = (Cliente) session.getAttribute("acessoCliente");
+		if(cliente == null){
+			response.sendRedirect("index.jsp");
+		}else{
+			try {
+				IFachada fachada = Fachada.getInstance();
+				
+				String mensagem="";
+				
+				cliente = fachada.pesquisarCliente(cliente);
+				String confirmarSenha = encriptar(request.getParameter("confirmarSenha"));
+				String senhaAtual = encriptar(request.getParameter("senhaAtual"));
+				String senhaBanco = cliente.getSenha();
+				
+				if(senhaAtual.equals(senhaBanco)){
+					cliente.setSenha(confirmarSenha);
+					fachada.alterarCliente(cliente);
+					
+					mensagem = "Senha editada com sucesso!";
+				}else{
+					mensagem = "Ocorreu um erro e a senha não foi editada!";
+				}
+				request.setAttribute("mensagem",mensagem);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("editarSenhaCliente.jsp");
+				dispatcher.forward(request, response);		
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static String encriptar(String senha) {     
+        try {     
+             MessageDigest digest = MessageDigest.getInstance("MD5");      
+             digest.update(senha.getBytes());      
+             BASE64Encoder encoder = new BASE64Encoder ();      
+             return encoder.encode (digest.digest ());      
+        } catch (NoSuchAlgorithmException ns) {     
+             ns.printStackTrace ();      
+             return senha;      
+        }      
+   }      
+
+}
