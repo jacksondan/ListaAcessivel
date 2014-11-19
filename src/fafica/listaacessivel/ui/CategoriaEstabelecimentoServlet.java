7df67987edb234d14ee0a247a9429ae60e4fa777
lw4z <ivanvalentimsantos@gmail.com>
2014-11-18 21:32:43 -0300

    Implementação do servlet categoriaEstabelecimento

---
+++ src/fafica/listaacessivel/ui/CategoriaEstabelecimentoServlet.java
@@ -0,0 +1,76 @@
+package fafica.listaacessivel.ui;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+import fafica.listaacessivel.negocios.Fachada;
+import fafica.listaacessivel.negocios.IFachada;
+import fafica.listaacessivel.negocios.entidades.Cliente;
+
+/**
+ * Servlet implementation class CategoriaEstabelecimentoServlet
+ */
+@WebServlet("/CategoriaEstabelecimentoServlet")
+public class CategoriaEstabelecimentoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CategoriaEstabelecimentoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		HttpSession session = request.getSession();
+		Cliente cliente = (Cliente) session.getAttribute("acessoCliente");
+		if(cliente == null){
+			response.sendRedirect("index.jsp");
+		}else{
+			try {
+				String categoria = request.getParameter("categoria");
+				IFachada fachada = Fachada.getInstance();
+				
+				
+				cliente = fachada.pesquisarCliente(cliente);
+				
+				request.setAttribute("editarCliente", cliente);
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("editarCliente.jsp");
+				requestDispatcher.forward(request, response);
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+	}
+
+}
