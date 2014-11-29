46ea4b6af4824dffd84663d89b778f72d251bdff
lw4z <ivanvalentimsantos@gmail.com>
2014-11-28 22:55:32 -0300

    Implementação do servlet editarsenhaadministrador e implementações das funcionalidades

--- src/fafica/listaacessivel/ui/EditarSenhaAdministradorServlet.java
+++ src/fafica/listaacessivel/ui/EditarSenhaAdministradorServlet.java
@@ -13,20 +13,20 @@ import javax.servlet.http.HttpSession;
 
 import fafica.listaacessivel.negocios.Fachada;
 import fafica.listaacessivel.negocios.IFachada;
-import fafica.listaacessivel.negocios.entidades.Funcionario;
+import fafica.listaacessivel.negocios.entidades.Administrador;
 import fafica.listaacessivel.ui.util.CriptografiaSenha;
 
 /**
- * Servlet implementation class EditarSenhaFuncionarioServlet
+ * Servlet implementation class EditarSenhaAdministradorServlet
  */
-@WebServlet("/EditarSenhaFuncionarioServlet")
-public class EditarSenhaFuncionarioServlet extends HttpServlet {
+@WebServlet("/EditarSenhaAdministradorServlet")
+public class EditarSenhaAdministradorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public EditarSenhaFuncionarioServlet() {
+    public EditarSenhaAdministradorServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -36,8 +36,8 @@ public class EditarSenhaFuncionarioServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		Funcionario funcionario = (Funcionario) session.getAttribute("acessoFuncionario");
-		if(funcionario==null){
+		Administrador administrador = (Administrador) session.getAttribute("acessoAdministrador");
+		if(administrador == null){
 			String mensagem = "Sessão expirada!";
 			request.setAttribute("mensagem", mensagem);
 			RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
@@ -45,8 +45,9 @@ public class EditarSenhaFuncionarioServlet extends HttpServlet {
 		}else{
 			String mensagem = "Senha editada com sucesso!";
 			request.setAttribute("mensagem", mensagem);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("editarSenhaFuncionario.jsp");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("editarSenhaAdministrador.jsp");
 			dispatcher.forward(request, response);
+			
 		}
 	}
 
@@ -55,34 +56,35 @@ public class EditarSenhaFuncionarioServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		Funcionario funcionario = (Funcionario) session.getAttribute("acessoFuncionario");
-		if(funcionario==null){
+		Administrador administrador = (Administrador) session.getAttribute("acessoAdministrador");
+		if(administrador == null){
 			String mensagem = "Sessão expirada!";
 			request.setAttribute("mensagem", mensagem);
 			RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
 			dispatcher.forward(request, response);
+			//response.sendRedirect("index.jsp");
 		}else{
 			try {
 				IFachada fachada = Fachada.getInstance();
 				
 				String mensagem="";
 				
-				funcionario = fachada.pesquisarFuncionario(funcionario);
+				administrador = fachada.pesquisarAdministrador(administrador);
 				String senhaNova= CriptografiaSenha.encriptar(request.getParameter("senhaNova"));
 				String confirmarSenha = CriptografiaSenha.encriptar(request.getParameter("confirmarSenha"));
 				String senhaAtual = CriptografiaSenha.encriptar(request.getParameter("senhaAtual"));
-				String senhaBanco = funcionario.getSenha();
+				String senhaBanco = administrador.getSenha();
 				
 				if(senhaAtual.equals(senhaBanco)&&senhaNova.equals(confirmarSenha)){
-					funcionario.setSenha(confirmarSenha);
-					fachada.alterarFuncionario(funcionario);
+					administrador.setSenha(confirmarSenha);
+					fachada.alterarAdministrador(administrador);
 					
 					mensagem = "Senha editada com sucesso!";
 				}else{
 					mensagem = "Sua Senha não foi alterada, por favor tente novamente.";
 				}
 				request.setAttribute("mensagem",mensagem);
-				RequestDispatcher dispatcher = request.getRequestDispatcher("editarSenhaFuncionario.jsp");
+				RequestDispatcher dispatcher = request.getRequestDispatcher("editarSenhaAdministrador.jsp");
 				dispatcher.forward(request, response);
 						
 			} catch (ClassNotFoundException | SQLException e) {
@@ -90,7 +92,6 @@ public class EditarSenhaFuncionarioServlet extends HttpServlet {
 				e.printStackTrace();
 			}
 		}
-	
 	}
 
 }
