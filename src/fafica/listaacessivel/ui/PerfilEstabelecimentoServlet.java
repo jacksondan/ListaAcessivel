5b7ebe46ae7f2c6570507fa74b0199ed4c663972
lw4z <ivanvalentimsantos@gmail.com>
2014-11-29 11:57:30 -0300

    Implementações no servlet PerfilEstabelecimento

--- src/fafica/listaacessivel/ui/PerfilEstabelecimentoServlet.java
+++ src/fafica/listaacessivel/ui/PerfilEstabelecimentoServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 
 import fafica.listaacessivel.negocios.Fachada;
 import fafica.listaacessivel.negocios.IFachada;
+import fafica.listaacessivel.negocios.entidades.Administrador;
 import fafica.listaacessivel.negocios.entidades.Estabelecimento;
 
 /**
@@ -21,59 +22,77 @@ import fafica.listaacessivel.negocios.entidades.Estabelecimento;
 @WebServlet("/PerfilEstabelecimentoServlet")
 public class PerfilEstabelecimentoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PerfilEstabelecimentoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-					
-			HttpSession session = request.getSession();
-			Estabelecimento estabelecimento = (Estabelecimento) session.getAttribute("acessoEstabelecimento");
-			
-			if(estabelecimento == null){
-				String mensagem = "Sessão expirada!";
-				request.setAttribute("mensagem", mensagem);
-				RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-				dispatcher.forward(request, response);
-				//response.sendRedirect("index.jsp");
-			}else{
-				
-					
-					
-					try {
-					IFachada fachada;fachada = Fachada.getInstance();
-						
-					estabelecimento = fachada.pesquisarEstabelecimento(estabelecimento);
-					
-					request.setAttribute("estabelecimento", estabelecimento);
-					RequestDispatcher requestDispatcher = request.getRequestDispatcher("perfilEstabelecimento.jsp");
-					requestDispatcher.forward(request, response);
-					} catch (ClassNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-				
-			}
-	
+	public PerfilEstabelecimentoServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Administrador administrador = (Administrador) session.getAttribute("acessoEstabelecimento");
+		Estabelecimento estabelecimento = (Estabelecimento) session.getAttribute("acessoEstabelecimento");
+
+		if (administrador == null && estabelecimento == null) {
+			String mensagem = "Sessão expirada!";
+			request.setAttribute("mensagem", mensagem);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+			dispatcher.forward(request, response);
+		}else if(administrador != null && estabelecimento == null){
+			try {
+				IFachada fachada;
+				fachada = Fachada.getInstance();
+				
+				int id_estabelecimento = Integer.parseInt(request.getParameter("id_estabelecimento"));
+				estabelecimento = new Estabelecimento();
+				estabelecimento.setId_estabelecimento(id_estabelecimento);
+				estabelecimento = fachada.pesquisarEstabelecimento(estabelecimento);
+
+				request.setAttribute("estabelecimento", estabelecimento);
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("perfilEstabelecimento.jsp");
+				requestDispatcher.forward(request, response);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}else if(administrador == null && estabelecimento != null){
+			try {
+				IFachada fachada;
+				fachada = Fachada.getInstance();
+				
+				estabelecimento = fachada.pesquisarEstabelecimento(estabelecimento);
+
+				request.setAttribute("estabelecimento", estabelecimento);
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("perfilEstabelecimento.jsp");
+				requestDispatcher.forward(request, response);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
