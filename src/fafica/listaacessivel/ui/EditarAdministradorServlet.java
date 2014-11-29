aab5dd7522e8a83f3ed82ee8b25d2050a3ee7d66
lw4z <ivanvalentimsantos@gmail.com>
2014-11-28 23:22:44 -0300

    Implementação do servlet editar administrador

--- src/fafica/listaacessivel/ui/EditarAdministradorServlet.java
+++ src/fafica/listaacessivel/ui/EditarAdministradorServlet.java
@@ -1,11 +1,22 @@
 package fafica.listaacessivel.ui;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fafica.listaacessivel.negocios.Fachada;
+import fafica.listaacessivel.negocios.IFachada;
+import fafica.listaacessivel.negocios.entidades.Administrador;
+import fafica.listaacessivel.negocios.entidades.Cliente;
+import fafica.listaacessivel.negocios.entidades.Endereco;
 
 /**
  * Servlet implementation class EditarAdministrador
@@ -26,14 +37,62 @@ public class EditarAdministradorServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		Administrador administrador = (Administrador) session.getAttribute("acessoAdministrador");
+		if(administrador == null){
+			String mensagem = "Sessão expirada!";
+			request.setAttribute("mensagem", mensagem);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+			dispatcher.forward(request, response);
+		}else{
+			try {
+				IFachada fachada = Fachada.getInstance();
+				administrador = fachada.pesquisarAdministrador(administrador);
+				
+				request.setAttribute("editarAdministrador", administrador);
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("editarAdministrador.jsp");
+				requestDispatcher.forward(request, response);
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		Administrador administrador = (Administrador) session.getAttribute("acessoAdministrador");
+		if(administrador == null){
+			response.sendRedirect("index.jsp");
+		}else{
+			try {
+				IFachada fachada = Fachada.getInstance();
+				
+				int id_administrador = administrador.getId_usuario();
+				String nome = request.getParameter("nome");
+				String cpf = request.getParameter("cpf");
+				String email = request.getParameter("email");
+				String senha = administrador.getSenha();
+				
+				Administrador administrador2 = new Administrador(id_administrador, nome, email, cpf, senha);
+								
+				fachada.alterarAdministrador(administrador2);
+				
+				response.sendRedirect("PerfilAdministradorServlet");
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
