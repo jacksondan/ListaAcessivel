a36b5ee0a863c8639a70e7256a26cea851434c1e
jackson <jackson.daniel.almeida@gmail.com>
2014-12-01 18:04:19 -0300

    Servlet do criar lista passo 3

--- src/fafica/listaacessivel/ui/CriarListaPasso3Servlet.java
+++ src/fafica/listaacessivel/ui/CriarListaPasso3Servlet.java
@@ -1,11 +1,22 @@
 package fafica.listaacessivel.ui;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fafica.listaacessivel.negocios.Fachada;
+import fafica.listaacessivel.negocios.IFachada;
+import fafica.listaacessivel.negocios.entidades.Cliente;
+import fafica.listaacessivel.negocios.entidades.Estabelecimento;
+import fafica.listaacessivel.negocios.entidades.Produto;
 
 /**
  * Servlet implementation class CriarListaPasso3Servlet
@@ -26,7 +37,36 @@ public class CriarListaPasso3Servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		Cliente cliente = (Cliente) session.getAttribute("acessoCliente");
+		if(cliente == null){
+			String mensagem = "Sessão expirada!";
+			request.setAttribute("mensagem", mensagem);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+			dispatcher.forward(request, response);
+			//response.sendRedirect("index.jsp");
+		}else{
+			try {
+				IFachada fachada = Fachada.getInstance();
+				
+				int id_estabelecimento = Integer.parseInt(request.getParameter("id_estabelecimento"));
+				Estabelecimento estabelecimento= new Estabelecimento();
+				estabelecimento.setId_estabelecimento(id_estabelecimento);
+				
+				List<Produto> listaprodutos = fachada.listarProdutosDoEstababelecimento(estabelecimento);
+				
+				request.setAttribute("listaprodutos",listaprodutos);
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("criarListaPasso03.jsp");
+				requestDispatcher.forward(request, response);
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
