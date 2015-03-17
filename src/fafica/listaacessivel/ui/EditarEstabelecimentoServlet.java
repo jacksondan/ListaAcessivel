2231f35111ddba69b0eec656d816b51a49508d73
lw4z <ivanvalentimsantos@gmail.com>
2015-03-16 21:25:23 -0300

    Modificações no editar estabelecimento

--- src/fafica/listaacessivel/ui/EditarEstabelecimentoServlet.java
+++ src/fafica/listaacessivel/ui/EditarEstabelecimentoServlet.java
@@ -87,7 +87,55 @@ public class EditarEstabelecimentoServlet extends HttpServlet {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
 			dispatcher.forward(request, response);
 		}else if(administrador != null && estabelecimento == null){
-/*			try {
+			try {
+				IFachada fachada = Fachada.getInstance();
+				ArrayList<String> telefones = new ArrayList<String>();
+								
+				estabelecimento = fachada.pesquisarEstabelecimento(estabelecimento);
+				
+				//int id_estabelecimento = estabelecimento.getId_estabelecimento();
+				//String nome_fantasia = request.getParameter("nome_fantasia");
+				//String nome_juridico = request.getParameter("nome_juridico");
+				//String categoria = request.getParameter("categoria");
+				//String cnpj = request.getParameter("cnpj");
+				String email = request.getParameter("email");
+				String senha = estabelecimento.getSenha();
+				telefones.add(request.getParameter("telefone1"));
+				telefones.add(request.getParameter("telefone2"));
+				//String rua = request.getParameter("rua");
+				//String numero = request.getParameter("numero");
+				//String complemento = request.getParameter("complemento");
+				//String bairro = request.getParameter("bairro");
+				//String cidade = request.getParameter("cidade");
+				//String estado = request.getParameter("estado");
+				//String cep = request.getParameter("cep");
+				//String referencia = request.getParameter("referencia");
+				//Administrador administrador = estabelecimento.getAdministrador();
+				
+				//Endereco endereco = new Endereco(rua, bairro, numero, complemento, referencia, cidade, estado, cep);
+				
+				//Estabelecimento entidade = new Estabelecimento(id_estabelecimento,nome_fantasia,nome_juridico,email,categoria,cnpj,endereco,senha,telefones,administrador);
+				
+				estabelecimento.setEmail(email);
+				estabelecimento.setSenha(senha);
+				estabelecimento.setTelefones(telefones);
+				
+				fachada.alterarEstabelecimento(estabelecimento);
+				
+				String mensagem = "Edição realizada com sucesso!!";
+				request.setAttribute("mensagem", mensagem);
+				//Tela de edição para o estabelecimento
+				RequestDispatcher dispatcher = request.getRequestDispatcher("perfilEstabelecimento.jsp");
+				dispatcher.forward(request, response);
+				
+				response.sendRedirect("PerfilEstabelecimentoServlet");
+						
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(administrador != null && estabelecimento == null){
+			try {
 				IFachada fachada = Fachada.getInstance();
 				ArrayList<String> telefones = new ArrayList<String>();
 				
@@ -99,10 +147,10 @@ public class EditarEstabelecimentoServlet extends HttpServlet {
 				String nome_juridico = request.getParameter("nome_juridico");
 				String categoria = request.getParameter("categoria");
 				String cnpj = request.getParameter("cnpj");
-				String email = request.getParameter("email");
-				String senha = estabelecimento.getSenha();
-				telefones.add(request.getParameter("telefone1"));
-				telefones.add(request.getParameter("telefone2"));
+				//String email = request.getParameter("email");
+				//String senha = estabelecimento.getSenha();
+				//telefones.add(request.getParameter("telefone1"));
+				//telefones.add(request.getParameter("telefone2"));
 				String rua = request.getParameter("rua");
 				String numero = request.getParameter("numero");
 				String complemento = request.getParameter("complemento");
@@ -111,45 +159,29 @@ public class EditarEstabelecimentoServlet extends HttpServlet {
 				String estado = request.getParameter("estado");
 				String cep = request.getParameter("cep");
 				String referencia = request.getParameter("referencia");
-				Administrador administrador = estabelecimento.getAdministrador();
+				Administrador administrador2 = estabelecimento.getAdministrador();
 				
 				Endereco endereco = new Endereco(rua, bairro, numero, complemento, referencia, cidade, estado, cep);
 				
-				Estabelecimento entidade = new Estabelecimento(id_estabelecimento,nome_fantasia,nome_juridico,email,categoria,cnpj,endereco,senha,telefones,administrador);
-				//entidade.setTelefones(telefones);
-				
-				fachada.alterarEstabelecimento(entidade);
-				
-				response.sendRedirect("PerfilEstabelecimentoServlet");
-						
-			} catch (ClassNotFoundException | SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-		}else if(administrador == null && estabelecimento != null){
-			try {
-				IFachada fachada = Fachada.getInstance();
-				ArrayList<String> telefones = new ArrayList<String>();
-				
-				/*
-				 exemplo1:
-				 estabelecimento.setEmail(request.getParameter("email");)
-				 
-				 exemplo2:
-				 telefones.add(request.getParameter("telefone1");)
-				 telefones.add(request.getParameter("telefone2");)
-				 
-				 estabelecimento.setTelefones(telefones)
-				 
-				 
-				 */
+				//Estabelecimento entidade = new Estabelecimento(id_estabelecimento,nome_fantasia,nome_juridico,email,categoria,cnpj,endereco,senha,telefones,administrador);
+								
+				estabelecimento.setEndereco(endereco);
+				estabelecimento.setNome_fantasia(nome_fantasia);
+				estabelecimento.setNome_juridico(nome_juridico);
+				estabelecimento.setCategoria(categoria);
+				estabelecimento.setCnpj(cnpj);
+				estabelecimento.setAdministrador(administrador2);
+				estabelecimento.setTelefones(telefones);
 				
 				fachada.alterarEstabelecimento(estabelecimento);
 				
-				String mensagem = "Edi��o realizada com sucesso!!";
+				String mensagem = "Edição realizada com sucesso!!";
 				request.setAttribute("mensagem", mensagem);
+				//Tela de edição para o administrador
 				RequestDispatcher dispatcher = request.getRequestDispatcher("perfilEstabelecimento.jsp");
 				dispatcher.forward(request, response);
+				
+				response.sendRedirect("PerfilEstabelecimentoServlet");
 				
 			} catch (ClassNotFoundException | SQLException e) {
 				// TODO Auto-generated catch block
