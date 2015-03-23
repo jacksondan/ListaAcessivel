72148a6799105af2d30a1049754c79efb9665737
lw4z <ivanvalentimsantos@gmail.com>
2015-03-23 11:39:11 -0300

    Implementação de classe com método para remover acentuação

---
+++ src/fafica/listaacessivel/ui/util/Acentuacao.java
@@ -0,0 +1,14 @@
+package fafica.listaacessivel.ui.util;
+
+import java.text.Normalizer;
+
+public class Acentuacao {
+
+	public static String limparAcentuacao(String palavra)     {  
+		//palavra = palavra.replaceAll(" ","_");  
+		palavra = Normalizer.normalize(palavra, Normalizer.Form.NFD);  
+		palavra = palavra.replaceAll("[^\\p{ASCII}]", "");  
+	    return palavra;  
+	}  
+	
+}
