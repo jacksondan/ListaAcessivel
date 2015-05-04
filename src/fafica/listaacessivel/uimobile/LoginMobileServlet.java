4c381b39ddce9160b2f3d07334eab061071490d5
Jackson <jackson.daniel.almeida@gmail.com>
2015-05-03 23:25:32 -0300

    Comitando mais algumas classes de teste

---
+++ src/fafica/listaacessivel/uimobile/LoginMobileServlet.java
@@ -0,0 +1,51 @@
+package fafica.listaacessivel.uimobile;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class LoginMobileServlet
+ */
+@WebServlet("/LoginMobileServlet")
+public class LoginMobileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginMobileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Login login = new Login("teste@teste","fewfew");
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(login);
+		System.out.println(json);
+		
+		request.setAttribute("login", json);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("#");
+		requestDispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
