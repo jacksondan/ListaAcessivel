207598d64c6ad32fef8ee4be90429d3e2ecb70b7
jackson <jackson.daniel.almeida@gmail.com>
2015-05-11 18:13:48 -0300

    Servlet mobile criar lista passo 1

---
+++ src/fafica/listaacessivel/uimobile/CriarListaPasso1MobileServlet.java
@@ -0,0 +1,42 @@
+package fafica.listaacessivel.uimobile;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CriarListaPasso1MobileServlet
+ */
+@WebServlet("/CriarListaPasso1MobileServlet")
+public class CriarListaPasso1MobileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CriarListaPasso1MobileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String categoria=request.getParameter("categoria");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
