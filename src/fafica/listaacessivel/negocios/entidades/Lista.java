28c8c4aaf9928dca1be6657c66057f66fd90b385
jackson <jackson.daniel.almeida@gmail.com>
2015-05-13 16:38:34 -0300

    Ajustes na classe lista

--- src/fafica/listaacessivel/negocios/entidades/Lista.java
+++ src/fafica/listaacessivel/negocios/entidades/Lista.java
@@ -19,9 +19,7 @@ public class Lista {
 		
 	private SimpleDateFormat dmy = new SimpleDateFormat("d/M/y");;
 	
-	public Lista(){
-		
-	}
+	public Lista() { }
 	
 	public Lista(String descricao, String situacao, Cliente cliente, Estabelecimento estabelecimento, List<Produto> produtos) {
 		this.descricao = descricao;
@@ -30,11 +28,6 @@ public class Lista {
 		this.estabelecimento = estabelecimento;
 		this.produtos = produtos;
 		this.data_criacao = dmy.format(new Date());
-		
-/*		if(produtos != null){
-			this.quantidade_total = calcularQuantidadeTotal();
-			this.valor_total = calcularValorTotal();
-		}*/
 	}
 	
 	public Lista(int id_lista, String descricao, String situacao,
@@ -48,11 +41,6 @@ public class Lista {
 		this.estabelecimento = estabelecimento;
 		this.produtos = produtos;
 		this.data_alteracao = dmy.format(new Date());
-		
-/*		if(produtos != null){
-			this.quantidade_total = calcularQuantidadeTotal();
-			this.valor_total = calcularValorTotal();
-		}*/
 		
 	}
 
@@ -72,22 +60,6 @@ public class Lista {
 		this.estabelecimento = estabelecimento;
 		this.produtos = produtos;
 	}
-	
-/*	private int calcularQuantidadeTotal(){
-		int quantidade = 0;
-		for(Produto produto : this.produtos){
-			quantidade += produto.getQuantidade();
-		}
-		return quantidade;
-	}
-	
-	private float calcularValorTotal(){
-		float valor = 0.0f;
-		for(Produto produto : this.produtos){
-			valor += (produto.getValor() * produto.getQuantidade());
-		}
-		return valor;
-	}*/
 
 	public int getId_lista() {
 		return id_lista;
@@ -168,14 +140,5 @@ public class Lista {
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
 	}
-
-	public SimpleDateFormat getDmy() {
-		return dmy;
-	}
-
-	public void setDmy(SimpleDateFormat dmy) {
-		this.dmy = dmy;
-	}
-
 
 }
