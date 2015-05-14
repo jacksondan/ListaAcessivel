bbf6f9d660dc5d731b45fa8fc99bffdcc8fd16d0
jackson <jackson.daniel.almeida@gmail.com>
2015-05-14 11:24:26 -0300

    Servlet do passo 3

--- src/fafica/listaacessivel/uimobile/CriarListaPasso3MobileServlet.java
+++ src/fafica/listaacessivel/uimobile/CriarListaPasso3MobileServlet.java
@@ -1,11 +1,19 @@
 package fafica.listaacessivel.uimobile;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import fafica.listaacessivel.negocios.Fachada;
+import fafica.listaacessivel.negocios.IFachada;
+import fafica.listaacessivel.negocios.entidades.Lista;
 
 /**
  * Servlet implementation class CriarListaPasso3MobileServlet
@@ -13,6 +21,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CriarListaPasso3MobileServlet")
 public class CriarListaPasso3MobileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private IFachada fachada;
+	private Lista lista;
+	private Gson gson;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -26,7 +38,22 @@ public class CriarListaPasso3MobileServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String jsonLista = request.getParameter("jsonLista");
+		
+		try {
+			fachada = Fachada.getInstance();
+			
+			gson = new Gson();
+			lista = gson.fromJson(jsonLista, Lista.class);
+			
+			fachada.adicionarLista(lista);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
